e0767b3101562e268c15e0cce7c3d0fa3c4da6e4
Senrix917 <86194236+Senrix917@users.noreply.github.com>
2023-06-30 17:04:52 +0200

    Update NameRecognition.ts

--- Client/src/Services/NameRecognition.ts
+++ Client/src/Services/NameRecognition.ts
@@ -1,36 +1,22 @@
 import NameNotFoundError from "../Errors/NameNotFoundError";
+import settings from "../Settings/settings";
 
 export const nameRecognition = (str: string): { name: string; distance: number; } => {
   const matrix: number[][] = [];
   const distances: number[] = [];
-  let actualName: string = "";
-  const names = [
-    "Cammy",
-    "Lily",
-    "Zangief",
-    "JP",
-    "Dee_Jay",
-    "Blanka",
-    "E.Honda",
-    "Juri",
-    "Ken",
-    "Ryu",
-    "Kimberly",
-    "Luke",
-    "Chun-Li",
-    "Guile",
-    "Marisa",
-    "Manon",
-    "Dhalsim",
-    "Jamie",
-  ];
+  let actualName: string = ""
+  const names:string [] = settings.names;
+  
   /*
    * goes through the list of names and calculates the distance between each name and the input String
    * and returns the name that is closest to the input
    */
   if(str.length > 20){
     throw new NameNotFoundError(str);
-  } else{
+  }else if (str.length === 0){
+    throw new NameNotFoundError(str)
+  }
+   else{
   names.forEach((name) => {
     // creates a matrix for the Levenshtein-Distance
     for (let i = 0; i <= name.length; i++) {
